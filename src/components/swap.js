053e876093996c7a3749e4d5d8cdf65249d4322a
leedc0101 <leedc0101@naver.com>
2021-07-23 16:15:46 +0900

    Swap Button v0.1

--- src/components/swap.js
+++ src/components/swap.js
@@ -1,35 +1,48 @@
 import {useWeb3React} from "@web3-react/core";
 import React from "react";
-import {Fetcher, Pair, Percent, Route, Token, TokenAmount, Trade, TradeType, WETH} from "@uniswap/sdk";
+// import {Fetcher, Pair, Percent, Route, Token, TokenAmount, Trade, TradeType, WETH} from "@uniswap/sdk";
+import { Pair, Token, WETH } from "@uniswap/sdk"
 import { ethers } from 'ethers'
 
 function SwapButton() {
-    const { chainId, account, activate, active, library } = useWeb3React()
+    const { chainId, account, active, library } = useWeb3React()
 
-    const abi = [{"inputs":[{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"deadline","type":"uint256"}],"name":"swapExactETHForTokens","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"payable","type":"function"}]
+    const swapABI = ['function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)']
+    const tokenABI = ['function approve(address _spender, uint256 amount) external returns (bool)']
     const privateKey = '8a2d8715d6d5031a7193266270d2f8ba40db63654241453bc3d3999f1ecf2805'
-    const wallet = new ethers.Wallet(privateKey, library)
-    const contract = library ? new ethers.Contract('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',abi, wallet) : undefined
-
-    const tokenAddress = '0xd3A691C852CDB01E281545A27064741F0B7f6825' // must be checksummed
+    const tokenAddress = '0xd3A691C852CDB01E281545A27064741F0B7f6825'
+    const routerAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
     const decimals = 18
 
     async function swap(): Promise<Pair> {
+        const wallet = new ethers.Wallet(privateKey, library)
+
+        const routerContract = library ? new ethers.Contract(routerAddress,swapABI, wallet) : undefined
+        const tokenContract = library ? new ethers.Contract(tokenAddress, tokenABI, wallet) : undefined
+        console.log(tokenContract)
+
+        let tx1 = await tokenContract.approve(routerAddress, 100000000,{gasLimit:100000})
+        await tx1.wait()
+        console.log(tx1)
+
         const WBTC = new Token(chainId, tokenAddress, decimals)
-        const pair = WBTC ? await Fetcher.fetchPairData(WBTC, WETH[WBTC.chainId]) : undefined
-        const route = WBTC ? new Route([pair], WETH[WBTC.chainId]) : undefined
-        const amountIn = '10000000000000000'
-        const trade = WBTC ? new Trade(route, new TokenAmount(WETH[WBTC.chainId], amountIn), TradeType.EXACT_INPUT) : undefined
+        // const pair = WBTC ? await Fetcher.fetchPairData(WBTC, WETH[WBTC.chainId]) : undefined
+        // const route = WBTC ? new Route([pair], WETH[WBTC.chainId]) : undefined
+        // const amountIn = '20000000000000000'
+        // const trade = WBTC ? new Trade(route, new TokenAmount(WETH[WBTC.chainId], amountIn), TradeType.EXACT_INPUT) : undefined
+        // const slippageTolerance = new Percent('50', '10000') // 50 bips, or 0.50%
 
-        const slippageTolerance = new Percent('50', '10000') // 50 bips, or 0.50%
-
-        const amountOutMin = trade ? trade.minimumAmountOut(slippageTolerance).raw : undefined
+        const amountOutMin = '0'
         const path = WBTC ? [WETH[WBTC.chainId].address, WBTC.address] : undefined
         const to = account // should be a checksummed recipient address
         const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
-        const value = trade ? trade.inputAmount.raw : undefined
+        // const value = trade ? trade.inputAmount.raw : undefined
+        const value = '10000000000000000'
+        console.log(amountOutMin,path,to,deadline)
 
-        console.log(contract.estimateGas)
+        let tx2 = await routerContract.swapExactETHForTokens(amountOutMin, path, to, deadline,{value:value, gasLimit:10000000})
+        await tx2.wait()
+        console.log(tx2)
     }
 
     return (
@@ -47,3 +60,7 @@ function SwapButton() {
 
 
 export default SwapButton
+
+// await 빼고 해보자
+// react 프로젝트 하나 잡고 파악해보자
+
