e146f6110f20cf0c83fb69bb8fa9cc34dd2bb5f8
leedc0101 <leedc0101@naver.com>
2021-07-23 18:53:32 +0900

    add token & eth balance

--- src/components/wallet.js
+++ src/components/wallet.js
@@ -1,11 +1,28 @@
 import {useWeb3React} from "@web3-react/core";
-import React from "react";
+import React, {useState} from "react";
 import {injectedConnector} from "../connector/connector";
+import { ethers } from "ethers"
+import { UNI_ABI, UNI_ADDRESS} from "../constant";
 
 function Wallet() {
-    const { chainId, account, activate, active } = useWeb3React()
+    const { chainId, account, activate, active, library } = useWeb3React()
 
-    const onClick = () => {
+    /**
+     * @todo use React-Redux hook instead of React hook
+     */
+    const [ETHBalance, setETHBalance] = useState(0)
+    const [UNIBalance, setUNIBalance] = useState(0)
+
+    const tokenContract = new ethers.Contract(UNI_ADDRESS, UNI_ABI, library) // create token contract instance
+
+    library && library.getBalance(account)
+        .then( (result) => setETHBalance(ethers.utils.formatEther(result)) ) // update eth balance
+
+    library && tokenContract.balanceOf(account)
+        .then((result) => {setUNIBalance(ethers.utils.formatEther(result))}) // update token balance using balanceOf function in token contract
+
+
+    const onClick = function () {
         activate(injectedConnector)
     }
 
@@ -13,6 +30,8 @@ function Wallet() {
         <div>
             <div>ChainId: {chainId}</div>
             <div>Account: {account}</div>
+            <div>ETH Balance: {ETHBalance}</div>
+            <div>UNI Balance: {UNIBalance}</div>
             {active ? (
                 <div>Connected </div>
             ) : (
