516d215170b3fe31251477c2bcf24f9cfa102957
leedc0101 <leedc0101@naver.com>
2021-08-03 21:31:38 +0900

    Swap v0.3

--- src/components/swap.js
+++ src/components/swap.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {useWeb3React} from "@web3-react/core";
 import { ethers } from 'ethers'
 import { Pair, Token, WETH } from "@uniswap/sdk"
-import {UNI_ADDRESS, ROUTER_ADDRESS, ROUTER_ABI, UNI_ABI} from "../constant";
-import store from '../store'
+import {UNI_ADDRESS, ROUTER_ADDRESS, ROUTER_ABI, CONTRACT_ABI} from "../constant";
 import {updateETH, updateUNI} from "../actions";
+import {useDispatch} from "react-redux";
 
 function SwapButton() {
+    const dispatch = useDispatch()
     const { chainId, account, active, library } = useWeb3React()
+    const [pending, setPending] = useState(false)
 
     function swap(): Promise<Pair> {
         const signer = library.getSigner(account).connectUnchecked()
         const routerContract = new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, signer) // create contract instance
-        const tokenContract = new ethers.Contract(UNI_ADDRESS, UNI_ABI, library)
+
+        const tokenContract = new ethers.Contract(UNI_ADDRESS, CONTRACT_ABI, library)
 
         const UNI = new Token(chainId, UNI_ADDRESS, 18) // Get UNI Token Instance
-        const amountIn = '10000000000000000'
+        const amountIn = '10000000000000000' // 0.01 ETH
         const amountOutMin = '0'
         const path = [WETH[UNI.chainId].address, UNI.address]
         const to = account // Send to myself
@@ -23,23 +26,32 @@ function SwapButton() {
 
         routerContract.swapExactETHForTokens(amountOutMin, path, to, deadline, {value: amountIn, gasLimit: 1000000})
             .then( (result) => {
+                setPending(true)
                 result.wait().then( () => {
                     library && tokenContract.balanceOf(account)
-                        .then((result) => store.dispatch(updateUNI(ethers.utils.formatEther(result))))
+                        .then((result) => dispatch(updateUNI(ethers.utils.formatEther(result))))
                     library && library.getBalance(account)
-                        .then((result) => store.dispatch(updateETH(ethers.utils.formatEther(result))))
+                        .then((result) => dispatch(updateETH(ethers.utils.formatEther(result))))
+                    setPending(false)
                 })
             })
     }
 
     return (
         <div style={{marginTop:"10px"}}>
-            {active ? (
+            <div>
+                ------Swap------
+            </div>
+            { !pending ? (active ? (
                 <button type="button" onClick={swap}>
                     Swap
                 </button>
             ) : (
                 <></>
+            )) : (
+                <div>
+                    Pending...
+                </div>
             )}
         </div>
     )
