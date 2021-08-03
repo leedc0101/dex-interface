65f4055ef8fecf79afab6994f0db831111e69a99
leedc0101 <leedc0101@naver.com>
2021-08-03 21:34:12 +0900

    Add LP action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
-import { UPDATE_ETH, UPDATE_UNI } from "../constant/action-types";
+import { UPDATE_ETH, UPDATE_UNI, UPDATE_LP } from "../constant/action-types";
 
 const initialState = {
     ethBalance: 0,
-    uniBalance: 0
+    uniBalance: 0,
+    LPBalance: 0,
 }
 
 const rootReducer = (state = initialState, action) => {
@@ -11,6 +12,8 @@ const rootReducer = (state = initialState, action) => {
             return {...state, ethBalance : action.ethBalance}
         case UPDATE_UNI:
             return {...state, uniBalance : action.uniBalance}
+        case UPDATE_LP:
+            return {...state, LPBalance : action.LPBalance}
     }
 }
 
