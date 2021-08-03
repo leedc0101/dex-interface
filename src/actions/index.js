781fc6ba65b6075897c9d9db2ff084ce8e6eebd1
leedc0101 <leedc0101@naver.com>
2021-08-03 21:32:50 +0900

    Add LP action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { UPDATE_UNI, UPDATE_ETH } from "../constant/action-types";
+import { UPDATE_UNI, UPDATE_ETH, UPDATE_LP } from "../constant/action-types";
 
 export const updateETH = ethBalance => ({
     type: UPDATE_ETH,
@@ -8,4 +8,9 @@ export const updateETH = ethBalance => ({
 export const updateUNI = uniBalance => ({
     type: UPDATE_UNI,
     uniBalance: uniBalance
+})
+
+export const updateLP = LPBalance => ({
+    type: UPDATE_LP,
+    LPBalance: LPBalance
 })
--- src/constant/action-types.js
+++ src/constant/action-types.js
@@ -1,2 +1,3 @@
 export const UPDATE_ETH = "UPDATE_ETH"
 export const UPDATE_UNI = "UPDATE_UNI"
+export const UPDATE_LP = "UPDATE_LP"
