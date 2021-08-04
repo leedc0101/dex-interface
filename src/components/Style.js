a9507246a286cd81ad1359cc88ce2a4c2dfcc089
leedc0101 <leedc0101@naver.com>
2021-08-04 18:20:18 +0900

    Add styled components for layout

---
+++ src/components/Style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-direction: column;
+  gap: normal;
+`
+
+export const Text = styled.div`
+  text-align: center;
+`
