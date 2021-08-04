cb8148cd649baa50f693eca36e1b0dea616fc2b8
leedc0101 <leedc0101@naver.com>
2021-08-04 18:20:01 +0900

    Swap v0.3.1 (needs to fixed)

--- src/components/swap.js
+++ src/components/swap.js
@@ -1,28 +1,42 @@
 import React, {useState} from "react";
 import {useWeb3React} from "@web3-react/core";
 import { ethers } from 'ethers'
-import { Pair, Token, WETH } from "@uniswap/sdk"
-import {UNI_ADDRESS, ROUTER_ADDRESS, ROUTER_ABI, CONTRACT_ABI} from "../constant";
-import {updateETH, updateUNI} from "../actions";
-import {useDispatch} from "react-redux";
+import { Pair, WETH } from "@uniswap/sdk"
+import {TOKEN_ADDRESS, ROUTER_ADDRESS, ROUTER_ABI, ERC20_ABI} from "../constant";
+import {updateETH, updateExpect, updateInput, updateOutput, updateUNI} from "../actions";
+import {useDispatch, useSelector} from "react-redux";
+import {Text, Wrap} from "./Style";
 
 function SwapButton() {
     const dispatch = useDispatch()
     const { chainId, account, active, library } = useWeb3React()
     const [pending, setPending] = useState(false)
+    const input = useSelector(state => state?.input)
+    const expect = useSelector(state => state?.expect)
+
+    // Todo
+    // const output = useSelector(state => state?.output)
+
+    const signer = library ? library.getSigner(account).connectUnchecked() : undefined
+    const routerContract = library ? new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, signer) : undefined // create contract instance
+    const tokenContract = new ethers.Contract(TOKEN_ADDRESS, ERC20_ABI, library)
+
+    const amountIn = input !== undefined ? ethers.utils.parseEther(input) : undefined
+    const path = [WETH[chainId]?.address, TOKEN_ADDRESS]
+    const amountOutMin = '0'
+    const to = account // Send to myself
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
+
+    if(amountIn !== undefined && !amountIn.eq(0)){
+        routerContract.getAmountsOut(amountIn, path, {gasLimit:10000000})
+            .then((result)=>{
+                dispatch(updateExpect(ethers.utils.formatEther(result[1])))
+            })
+    } else {
+        dispatch(updateExpect('0'))
+    }
 
     function swap(): Promise<Pair> {
-        const signer = library.getSigner(account).connectUnchecked()
-        const routerContract = new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, signer) // create contract instance
-
-        const tokenContract = new ethers.Contract(UNI_ADDRESS, CONTRACT_ABI, library)
-
-        const UNI = new Token(chainId, UNI_ADDRESS, 18) // Get UNI Token Instance
-        const amountIn = '10000000000000000' // 0.01 ETH
-        const amountOutMin = '0'
-        const path = [WETH[UNI.chainId].address, UNI.address]
-        const to = account // Send to myself
-        const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
 
         routerContract.swapExactETHForTokens(amountOutMin, path, to, deadline, {value: amountIn, gasLimit: 1000000})
             .then( (result) => {
@@ -37,23 +51,56 @@ function SwapButton() {
             })
     }
 
+    function inputOnChange(e){
+        if(e.target.value.length !== 0)
+            dispatch(updateInput(e.target.value))
+        else
+            dispatch(updateInput('0'))
+    }
+
+    // Todo
+    // function outputOnChange(e){
+    //     dispatch(updateOutput(e.target.value))
+    // }
+
     return (
-        <div style={{marginTop:"10px"}}>
-            <div>
+        <Wrap style={{marginTop:"10px"}}>
+            <Text>
                 ------Swap------
-            </div>
+            </Text>
             { !pending ? (active ? (
-                <button type="button" onClick={swap}>
-                    Swap
-                </button>
+                <>
+                    <form>
+                        <label>
+                            <Text>
+                                Input(ETH) :
+                                <input type="text" onChange={inputOnChange}/>
+                            </Text>
+                        </label>
+                    </form>
+                    {/*<form>*/}
+                    {/*    <label>*/}
+                    {/*        <Text>*/}
+                    {/*            Output(UNI) :*/}
+                    {/*            <input type="text" onChange={outputOnChange}/>*/}
+                    {/*        </Text>*/}
+                    {/*    </label>*/}
+                    {/*</form>*/}
+                    <Text>
+                        예상 Output(UNI) : {expect}
+                    </Text>
+                    <button type="button" onClick={swap}>
+                        Swap
+                    </button>
+                </>
             ) : (
                 <></>
             )) : (
-                <div>
+                <Text>
                     Pending...
-                </div>
+                </Text>
             )}
-        </div>
+        </Wrap>
     )
 }
 
