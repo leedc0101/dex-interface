44fb7c04dfc7473554e2e4a5c5138606df94b609
leedc0101 <leedc0101@naver.com>
2021-08-04 18:20:41 +0900

    Refactor react-redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { UPDATE_UNI, UPDATE_ETH, UPDATE_LP } from "../constant/action-types";
+import {UPDATE_UNI, UPDATE_ETH, UPDATE_LP, UPDATE_INPUT, UPDATE_OUTPUT, UPDATE_EXPECT} from "../constant/action-types";
 
 export const updateETH = ethBalance => ({
     type: UPDATE_ETH,
@@ -13,4 +13,19 @@ export const updateUNI = uniBalance => ({
 export const updateLP = LPBalance => ({
     type: UPDATE_LP,
     LPBalance: LPBalance
+})
+
+export const updateInput = input => ({
+    type: UPDATE_INPUT,
+    input: input
+})
+
+export const updateOutput = output => ({
+    type: UPDATE_OUTPUT,
+    output: output
+})
+
+export const updateExpect = expect => ({
+    type: UPDATE_EXPECT,
+    expect: expect
 })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
-import { UPDATE_ETH, UPDATE_UNI, UPDATE_LP } from "../constant/action-types";
+import {UPDATE_ETH, UPDATE_UNI, UPDATE_LP, UPDATE_OUTPUT, UPDATE_INPUT, UPDATE_EXPECT} from "../constant/action-types";
 
 const initialState = {
-    ethBalance: 0,
-    uniBalance: 0,
-    LPBalance: 0,
+    ethBalance: "0",
+    uniBalance: "0",
+    LPBalance: "0",
+    input: "0",
+    output: "0",
+    expect: "0",
 }
 
 const rootReducer = (state = initialState, action) => {
@@ -14,6 +17,12 @@ const rootReducer = (state = initialState, action) => {
             return {...state, uniBalance : action.uniBalance}
         case UPDATE_LP:
             return {...state, LPBalance : action.LPBalance}
+        case UPDATE_INPUT:
+            return {...state, input : action.input}
+        case UPDATE_OUTPUT:
+            return {...state, output : action.output}
+        case UPDATE_EXPECT:
+            return {...state, expect: action.expect}
     }
 }
 
