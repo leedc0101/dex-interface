536d88221205b9113ddf87641c8943c59034a9ba
leedc0101 <leedc0101@naver.com>
2021-08-04 18:31:05 +0900

    Add action-types

--- src/constant/action-types.js
+++ src/constant/action-types.js
@@ -1,3 +1,6 @@
 export const UPDATE_ETH = "UPDATE_ETH"
 export const UPDATE_UNI = "UPDATE_UNI"
 export const UPDATE_LP = "UPDATE_LP"
+export const UPDATE_INPUT = "UPDATE_INPUT"
+export const UPDATE_OUTPUT = "UPDATE_OUTPUT"
+export const UPDATE_EXPECT = "UPDATE_EXPECT"
