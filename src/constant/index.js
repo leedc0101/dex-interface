a06968f6a772f3f57905340c0c6bb8c11922caaa
leedc0101 <leedc0101@naver.com>
2021-08-04 18:23:30 +0900

    Correct constant name

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,11 +1,12 @@
-export const UNI_ADDRESS = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
+export const TOKEN_ADDRESS = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 export const ROUTER_ABI = [
     'function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
     'function addLiquidityETH(address token, uint amountTokenDesired, uint amountTokenMin, uint amountETHMin,address to,uint deadline) external payable returns (uint amountToken, uint amountETH, uint liquidity)',
-    'function removeLiquidityETH(address token,uint liquidity, uint amountTokenMin,uint amountETHMin,address to, uint deadline) external returns (uint amountToken, uint amountETH)'
+    'function removeLiquidityETH(address token,uint liquidity, uint amountTokenMin,uint amountETHMin,address to, uint deadline) external returns (uint amountToken, uint amountETH)',
+    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)'
 ]
-export const CONTRACT_ABI = [
+export const ERC20_ABI = [
     'function balanceOf(address account) external view returns (uint)',
     'function approve(address spender, uint rawAmount) external returns (bool)'
 ]
