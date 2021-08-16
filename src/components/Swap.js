cedc438789f19bd6769a43d0b59d30358d71a941
leedc0101 <leedc0101@naver.com>
2021-08-17 01:10:15 +0900

    Swap v0.4

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -1,30 +1,39 @@
 import React, {useState} from "react";
 import {useWeb3React} from "@web3-react/core";
 import { ethers } from 'ethers'
-import { Pair, WETH } from "@uniswap/sdk"
-import {TOKEN_ADDRESS, ROUTER_ADDRESS, ROUTER_ABI, ERC20_ABI} from "../constant";
-import {updateETH, updateSwapExpect, updateSwapInput, updateUNI} from "../actions";
+import {Pair, WETH} from "@uniswap/sdk"
+import {ROUTER_ADDRESS, ROUTER_ABI, ERC20_ABI} from "../constant";
+import {updateTokenA, updateSwapExpect, updateSwapInput, updateTokenB} from "../actions";
 import {useDispatch, useSelector} from "react-redux";
 import {BorderWrap, Text} from "./style";
 import {MaxUint256} from "@ethersproject/constants";
 
-function SwapButton() {
-    const dispatch = useDispatch()
+
+/////////  Redux 로 approve랑 pending을 바꿔보자
+
+
+function Swap() {
     const { chainId, account, library } = useWeb3React()
-    const [pending, setPending] = useState(false)
-    const [approved, setApproved] = useState(false)
+
+    const pending = false
+    const [approvedA, setApprovedA] = useState(false)
+    const [approvedB, setApprovedB] = useState(false)
+
+    const dispatch = useDispatch()
     const input = useSelector(state => state?.swapInput)
     const expect = useSelector(state => state?.swapExpect)
+    const tokenAAddress = useSelector(state => state?.tokenAAddress)
+    const tokenBAddress = useSelector(state => state?.tokenBAddress)
 
-    // Todo
-    // const output = useSelector(state => state?.output)
 
     const signer = library.getSigner(account).connectUnchecked()
     const routerContract = new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, signer) // create contract instance
-    const tokenContract = new ethers.Contract(TOKEN_ADDRESS, ERC20_ABI, library)
+
+    const tokenAContract = tokenAAddress === WETH[chainId].address ? undefined : new ethers.Contract(tokenAAddress, ERC20_ABI, library)
+    const tokenBContract = tokenBAddress === WETH[chainId].address ? undefined : new ethers.Contract(tokenBAddress, ERC20_ABI, library)
 
     const amountIn = input !== undefined ? ethers.utils.parseEther(input) : undefined
-    const path = [WETH[chainId]?.address, TOKEN_ADDRESS]
+    const path = [tokenAAddress, tokenBAddress]
     const amountOutMin = '0'
     const to = account // Send to myself
     const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
@@ -38,37 +47,80 @@ function SwapButton() {
         dispatch(updateSwapExpect('0'))
     }
 
-    tokenContract.allowance(account, ROUTER_ADDRESS)
-        .then(result => {
-            if( result >= MaxUint256.div(100))
-                setApproved(true)
-        })
+    if(tokenAContract !== undefined)
+        tokenAContract.allowance(account, ROUTER_ADDRESS)
+            .then(result => {
+                if( result >= MaxUint256.div(100))
+                    !approvedA && setApprovedA(true)
+            })
+    else
+        !approvedA && setApprovedA(true)
 
-    function approve() {
-        tokenContract.approve(ROUTER_ADDRESS, MaxUint256)
+    if(tokenBContract !== undefined)
+        tokenBContract.allowance(account, ROUTER_ADDRESS)
+            .then(result => {
+                if( result >= MaxUint256.div(100))
+                    !approvedB && setApprovedB(true)
+            })
+    else
+        !approvedB && setApprovedB(true)
+
+    function approveA() {
+        tokenAContract.approve(ROUTER_ADDRESS, MaxUint256)
             .then((result) => {
-                setPending(true)
                 result.wait().then(() => {
-                    setPending(false)
-                    setApproved(true)
+                    setApprovedA(true)
                 })
             })
     }
 
-    function swap(): Promise<Pair> {
-        routerContract.swapExactETHForTokens(amountOutMin, path, to, deadline, {value: amountIn, gasLimit: 1000000})
+    function approveB() {
+        tokenBContract.approve(ROUTER_ADDRESS, MaxUint256)
+            .then((result) => {
+                result.wait().then(() => {
+                    setApprovedB(true)
+                })
+            })
+    }
+
+    function swapExactTokensForTokens(): Promise<Pair> {
+        routerContract.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline, {gasLimit: ethers.utils.hexlify(250000), gasPrice: ethers.utils.parseUnits('5', "gwei")})
             .then( (result) => {
-                setPending(true)
                 result.wait().then( () => {
-                    library && tokenContract.balanceOf(account)
-                        .then((result) => dispatch(updateUNI(ethers.utils.formatEther(result))))
-                    library && library.getBalance(account)
-                        .then((result) => dispatch(updateETH(ethers.utils.formatEther(result))))
-                    setPending(false)
+                    library.getBalance(account)
+                        .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+                    library.getBalance(account)
+                        .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+
                     dispatch(updateSwapInput('0'))
                 })
             })
     }
+
+    function swapExactETHForTokens(): Promise<Pair> {
+        routerContract.swapExactETHForTokens(amountOutMin, path, to, deadline, {value: amountIn, gasLimit: ethers.utils.hexlify(250000), gasPrice: ethers.utils.parseUnits('5', "gwei") })
+            .then( (result) => {
+                result.wait().then( () => {
+                    if(tokenAAddress !== WETH[chainId].address)
+                        tokenAContract.balanceOf(account)
+                            .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+                    else
+                        library.getBalance(account)
+                            .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+
+                    if(tokenBAddress !== WETH[chainId].address)
+                        tokenBContract.balanceOf(account)
+                            .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+                    else
+                        library.getBalance(account)
+                            .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+                    dispatch(updateSwapInput('0'))
+                })
+            })
+    }
+
+    const onClick = tokenAContract !== undefined && tokenBContract !== undefined ?  swapExactTokensForTokens : swapExactETHForTokens
+    console.log(onClick)
 
     function inputOnChange(e){
         if(e.target.value.length !== 0)
@@ -76,11 +128,6 @@ function SwapButton() {
         else
             dispatch(updateSwapInput('0'))
     }
-
-    // Todo
-    // function outputOnChange(e){
-    //     dispatch(updateOutput(e.target.value))
-    // }
 
     return (
         <BorderWrap style={{marginTop:"10px", border:"2px solid"}}>
@@ -92,23 +139,37 @@ function SwapButton() {
                     <form>
                         <label>
                             <Text>
-                                Input(ETH) :
+                                Input :
                                 <input type="text" onChange={inputOnChange}/>
                             </Text>
                         </label>
                     </form>
                     <Text>
-                        예상 Output(UNI) : {expect}
+                        Output : {expect}
                     </Text>
-                    { approved ? (
-                        <button style={{color:"green"}} type="button" onClick={swap}>
+                    { approvedA && approvedB ? (
+                        <button style={{color:"green"}} type="button" onClick={onClick}>
                             Swap
                         </button>
                     ):(
-                        <button style={{color:"red"}} type="button" onClick={approve}>
-                            Approve
-                        </button>
-                    )}
+                        approvedA ?
+                            <button style={{color:"red"}} type="button" onClick={approveB}>
+                                Approve B
+                            </button>
+                            : approvedB ?
+                            <button style={{color:"red"}} type="button" onClick={approveA}>
+                                Approve A
+                            </button> :
+                            <>
+                                <button style={{color:"red"}} type="button" onClick={approveA}>
+                                    Approve A
+                                </button>
+                                <button style={{color:"red"}} type="button" onClick={approveB}>
+                                Approve B
+                                </button>
+                            </>
+                        )
+                    }
                 </>
             ) : (
                 <Text>
@@ -119,6 +180,5 @@ function SwapButton() {
     )
 }
 
-
-export default SwapButton
+export default Swap
 
