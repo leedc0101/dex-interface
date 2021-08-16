fffe03985876e8f2309dbf9f2e3496799d727076
leedc0101 <leedc0101@naver.com>
2021-08-17 01:05:50 +0900

    Add ROUTER_ABI

--- src/constant/index.js
+++ src/constant/index.js
@@ -4,10 +4,12 @@ export const ROUTER_ABI = [
     'function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
     'function addLiquidityETH(address token, uint amountTokenDesired, uint amountTokenMin, uint amountETHMin,address to,uint deadline) external payable returns (uint amountToken, uint amountETH, uint liquidity)',
     'function removeLiquidityETH(address token,uint liquidity, uint amountTokenMin,uint amountETHMin,address to, uint deadline) external returns (uint amountToken, uint amountETH)',
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)'
+    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
 ]
 export const ERC20_ABI = [
     'function balanceOf(address account) external view returns (uint)',
     'function approve(address spender, uint rawAmount) external returns (bool)',
     'function allowance(address owner, address spender) public view returns (uint256)'
 ]
+
