b0c4f7e613d4875f6c043297c15d1b68ce3c0c0b
leedc0101 <leedc0101@naver.com>
2021-08-17 22:52:28 +0900

    Fix decimals error

--- src/components/TokenBalance.js
+++ src/components/TokenBalance.js
@@ -1,5 +1,5 @@
 import {BorderWrap, Text} from "./style";
-import React from "react";
+import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useWeb3React} from "@web3-react/core";
 import {ethers} from "ethers";
@@ -17,14 +17,30 @@ function TokenBalance() {
     const tokenAAddress = useSelector(state => state?.tokenAAddress)
     const tokenBAddress = useSelector(state => state?.tokenBAddress)
 
+    const [ADecimals,setADecimals] = useState(0)
+    const [BDecimals,setBDecimals] = useState(0)
+
 
     const { chainId, account, library } = useWeb3React()
 
     const tokenAContract = tokenAAddress === WETH[chainId].address ? undefined : new ethers.Contract(tokenAAddress, ERC20_ABI, library)
     const tokenBContract = tokenBAddress === WETH[chainId].address ? undefined : new ethers.Contract(tokenBAddress, ERC20_ABI, library)
 
-    const tokenA = tokenAContract === undefined ? new Token(chainId, WETH[chainId].address, 18) : new Token(chainId, tokenAAddress, 18)
-    const tokenB = tokenBContract === undefined ? new Token(chainId, WETH[chainId].address, 18) : new Token(chainId, tokenBAddress, 18)
+
+    tokenAContract?.decimals()
+        .then(result => {
+            if(ADecimals !== result)
+                setADecimals(result)
+        })
+
+    tokenBContract?.decimals()
+        .then(result => {
+            if(BDecimals !== result)
+                setBDecimals(result)
+        })
+
+    const tokenA = tokenAContract === undefined ? new Token(chainId, WETH[chainId].address, 18) : new Token(chainId, tokenAAddress, ADecimals)
+    const tokenB = tokenBContract === undefined ? new Token(chainId, WETH[chainId].address, 18) : new Token(chainId, tokenBAddress, BDecimals)
 
     const tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
     const pair = getCreate2Address(
@@ -32,26 +48,24 @@ function TokenBalance() {
         keccak256(['bytes'], [pack(['address', 'address'], [tokens[0].address, tokens[1].address])]),
         INIT_CODE_HASH
     )
-    console.log(pair)
     const pairTokenContract = new ethers.Contract(pair, ERC20_ABI, library)
 
     if(tokenAAddress !== WETH[chainId].address)
         tokenAContract.balanceOf(account)
-            .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+            .then((result) => dispatch(updateTokenA(ethers.utils.formatUnits(result,ADecimals))))
     else
         library.getBalance(account)
             .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
 
     if(tokenBAddress !== WETH[chainId].address)
         tokenBContract.balanceOf(account)
-            .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+            .then((result) => dispatch(updateTokenB(ethers.utils.formatUnits(result,BDecimals))))
     else
         library.getBalance(account)
             .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
 
     pairTokenContract?.balanceOf(account)
         .then((result) => {
-            console.log(result)
             if(ethers.utils.formatEther(result) !== LPBalance)
                 dispatch(updateLP(ethers.utils.formatEther(result)))
         })
