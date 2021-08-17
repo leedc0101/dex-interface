5ac3711afb80a5327818ea37eb2447c282b22f6b
leedc0101 <leedc0101@naver.com>
2021-08-17 22:51:45 +0900

    Add removeLiquidity function

--- src/components/RemoveLiquidity.js
+++ src/components/RemoveLiquidity.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {useWeb3React} from "@web3-react/core";
 import { ethers } from 'ethers'
 import { Pair, Token, WETH, FACTORY_ADDRESS, INIT_CODE_HASH } from "@uniswap/sdk"
-import {TOKEN_ADDRESS, ROUTER_ADDRESS, ROUTER_ABI, ERC20_ABI} from "../constant";
+import {ROUTER_ADDRESS, ROUTER_ABI, ERC20_ABI} from "../constant";
 import {updateTokenA, updateTokenB} from "../actions";
 import { pack, keccak256 } from "@ethersproject/solidity";
 import { getCreate2Address } from "@ethersproject/address";
@@ -11,17 +11,23 @@ import {BorderWrap, Text} from "./style";
 import {MaxUint256} from "@ethersproject/constants";
 
 function RemoveLiquidityButton() {
-    const dispatch = useDispatch()
-    const LPBalance = useSelector((state) => state?.LPBalance )
     const { chainId, account, library } = useWeb3React()
 
-    const [approved, setApproved] = useState(false)
     const [pending, setPending] = useState(false)
+    const [approved, setApproved] = useState(false)
+
+    const dispatch = useDispatch()
+    const LPBalance = useSelector((state) => state?.LPBalance )
+    const tokenAAddress = useSelector(state => state?.tokenAAddress)
+    const tokenBAddress = useSelector(state => state?.tokenBAddress)
 
     const signer = library?.getSigner(account).connectUnchecked()
+    const routerContract = new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, signer) // create contract instance
+    const tokenAContract = tokenAAddress === WETH[chainId].address ? undefined : new ethers.Contract(tokenAAddress, ERC20_ABI, library)
+    const tokenBContract = tokenBAddress === WETH[chainId].address ? undefined : new ethers.Contract(tokenBAddress, ERC20_ABI, library)
 
-    const tokenA = new Token(chainId, TOKEN_ADDRESS, 18)
-    const tokenB = new Token(chainId, WETH[chainId].address, 18)
+    const tokenA = new Token(chainId, tokenAAddress, 18)
+    const tokenB = new Token(chainId, tokenBAddress, 18)
     const tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
     const pair = getCreate2Address(
         FACTORY_ADDRESS,
@@ -33,8 +39,8 @@ function RemoveLiquidityButton() {
 
     pairTokenContract.allowance(account, ROUTER_ADDRESS)
         .then(result => {
-            if( result >= MaxUint256.div(100))
-                setApproved(true)
+            if( ethers.utils.formatEther(result) >= ethers.utils.formatEther(MaxUint256.div(100)) )
+                !approved && setApproved(true)
         })
 
     function approve() {
@@ -48,29 +54,62 @@ function RemoveLiquidityButton() {
             })
     }
 
+    const liquidity = ethers.utils.parseEther(LPBalance)
+    const amountTokenMin = '0'
+    const amountETHMin = '0'
+    const to = account // Send to myself
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
+
     function removeLiquidity(): Promise<Pair> {
-        const routerContract = new ethers.Contract(ROUTER_ADDRESS, ROUTER_ABI, signer) // create contract instance
-        const tokenContract = new ethers.Contract(TOKEN_ADDRESS, ERC20_ABI, library)
+        routerContract.removeLiquidity(
+            tokenAAddress,
+            tokenBAddress,
+            liquidity,
+            '0',
+            '0',
+            to,
+            deadline
+        ).then((result)=> {
+            setPending(true)
+            result.wait().then(()=>{
+                tokenAAddress.balanceOf(account)
+                    .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+                tokenBAddress.balanceOf(account)
+                    .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+                setPending(false)
+            })
+        })
+    }
 
-        const token = TOKEN_ADDRESS
-        const amountTokenDesired = ethers.utils.parseEther(LPBalance)
-        const amountTokenMin = '0'
-        const amountETHMin = '0'
-        const to = account // Send to myself
-        const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
-
-        routerContract.removeLiquidityETH(token, amountTokenDesired, amountTokenMin, amountETHMin, to, deadline, {gasLimit: 1000000})
-            .then((result) => {
+    function removeLiquidityETH(): Promise<Pair> {
+        routerContract.removeLiquidityETH(
+            tokenAContract === undefined ? tokenBAddress : tokenAAddress,
+            liquidity,
+            amountTokenMin,
+            amountETHMin,
+            to,
+            deadline,
+            {gasLimit: ethers.utils.hexlify(250000), gasPrice: ethers.utils.parseUnits('5', "gwei")}
+        ).then((result) => {
                 setPending(true)
                 result.wait().then( () => {
-                    tokenContract.balanceOf(account)
-                        .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
-                    library.getBalance(account)
-                        .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+                    if(tokenAContract === undefined){
+                        tokenBContract.balanceOf(account)
+                            .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+                        library.getBalance(account)
+                            .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+                    } else {
+                        tokenAContract.balanceOf(account)
+                            .then((result) => dispatch(updateTokenA(ethers.utils.formatEther(result))))
+                        library.getBalance(account)
+                            .then((result) => dispatch(updateTokenB(ethers.utils.formatEther(result))))
+                    }
                     setPending(false)
                 })
             })
     }
+
+    const onClick = tokenAContract === undefined || tokenBContract === undefined ? removeLiquidityETH : removeLiquidity
 
     return (
         <BorderWrap>
@@ -78,7 +117,7 @@ function RemoveLiquidityButton() {
                 ------Remove Liquidity------
             </Text>
             { !pending ?  (approved ? (
-                <button style={{color:"green"}} type="button" onClick={removeLiquidity}>
+                <button style={{color:"green"}} type="button" onClick={onClick}>
                     Remove Liquidity
                 </button>
             ) : (
