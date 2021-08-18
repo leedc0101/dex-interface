78475c8dd1dd7783954c124516732a28596f27fa
leedc0101 <leedc0101@naver.com>
2021-08-18 13:11:32 +0900

    Add font-family

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Main from "./components/Main";
 import {AutoColumn} from "./components/Column";
 import styled from 'styled-components'
 
-
-
 const Wrap = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
+  font-family: 'Spoqa Han Sans Neo', serif;
 `
 
 function App() {
