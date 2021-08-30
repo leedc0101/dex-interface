32a17aa7281d6ad7a30b8d8e43bbe100b9dd19f3
leedc0101 <leedc0101@naver.com>
2021-08-30 13:46:20 +0900

    Reduce

--- src/library/library.js
+++ src/library/library.js
@@ -1,9 +1,7 @@
 import { Web3Provider } from "@ethersproject/providers";
 
 function getLibrary(provider: any): Web3Provider {
-    const library = new Web3Provider(provider)
-    library.pollingInterval = 12000
-    return library
+    return new Web3Provider(provider)
 }
 
 export default getLibrary
