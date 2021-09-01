2531ca47f1220e41df9c7fc820753b4721b63eb0
leedc0101 <leedc0101@naver.com>
2021-09-02 00:21:41 +0900

    Add row

---
+++ src/components/Row.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Row = styled.div`
+  display: flex;
+  width: 100%;
+  justify-items: center;
+`
+
+export const RowBetween = styled(Row)`
+  justify-content: space-between;
+`
